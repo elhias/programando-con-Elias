7a9e52a1c45048e60e6981a67a1a06892f00d2b7
elhias <165832021+elhias@users.noreply.github.com>
2024-04-04 12:14:47 -0400

    Add files via upload
    Hola otra vez yo (jejejeje) bueno hoy les traje una pequeña guia para comenzar a programar con C++ y esta guia es para que ustedes creen su propia pagina web con C++ este simplemente es un ejemplo basico si qieres uno mas avanzado comparte este primero con tus amigos BYE

---
+++ index.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+//cuenta de github
+
+//https://github.com/elhias/programando-con-Elias
+
+int main(){
+std::cout << "Hola Bienvenido A Mi Pagina Web Creada con C++" << std::endl;
+
+string contenido = "Hola esta pagina web fue creada con C++ y algo de HTML";
+
+string mastexto = "Si quieres saber como e creado esta pagina ve a mi cuenta de GITHUB";
+
+string linea = "te dejare el codigo para que te guies y aprendas C++ que es super facil de aprender";
+
+string linea2 = "esto en realidad no es una pagina es como un pequeño texto de guia";
+
+string despedida = "me despido de ustedes bye saludos a todos";
+
+cout << contenido << endl;
+
+cout << mastexto << endl;
+
+cout << linea << endl;
+
+cout << linea2 << endl;
+
+cout << despedida << endl;
+
+return 0;
+}
